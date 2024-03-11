8db2e53a742ac31df8d6c79add26b2b2bbebcc04
JosaelPrz <josael.prz@gmail.com>
2024-03-11 12:22:50 -0700

    Include FAQ section to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,12 @@
 	import Header from '$lib/components/header.svelte';
 	import HeroSection from '$lib/components/hero-section.svelte';
 	import PricingSection from '$lib/components/pricing-section.svelte';
+	import FAQSection from '$lib/components/faq-section.svelte';
 </script>
 
 <Header />
 <main>
 	<HeroSection />
 	<PricingSection />
+	<FAQSection />
 </main>
